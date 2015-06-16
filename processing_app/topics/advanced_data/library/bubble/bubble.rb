d2b9cf55ba5fc8f3a8ed061c60221470915ede4a
monkstone <martin_p@lineone.net>
2015-06-16 11:20:47 +0100

    more examples

---
+++ processing_app/topics/advanced_data/library/bubble/bubble.rb
@@ -0,0 +1,50 @@
+# The bubble library, include BubbleStruct
+class Bubble
+  include Processing::Proxy
+
+  attr_reader :x, :y, :diameter, :name, :over
+
+  # Create  the Bubble
+  def initialize(x, y, diameter, name)
+    @x = x
+    @y = y
+    @diameter = diameter
+    @name = name
+    @over = false
+  end
+
+  # Checking if mouse is over the Bubble
+  def rollover(px, py)
+    d = dist(px, py, x, y)
+    @over = (d < diameter / 2.0) ? true : false
+  end
+
+  # Display the Bubble
+  def display
+    stroke(0)
+    stroke_weight(2)
+    noFill
+    ellipse(x, y, diameter, diameter)
+    return unless over
+    fill(0)
+    text_align(PConstants::CENTER)
+    text(name, x, y + diameter / 2 + 20)
+  end
+
+  def to_a
+    [x, y, diameter, name]
+  end
+
+  def to_hash
+    { 'position' => { 'x' => x, 'y' => y },
+      'diameter' => diameter,
+      'label' => name
+    }
+  end
+
+  def to_struct
+    BubbleStruct.new(x, y, diameter, name)
+  end
+end
+
+BubbleStruct = Struct.new(:x, :y, :diameter, :label)

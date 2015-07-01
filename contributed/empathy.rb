5dd846a8dc9c7873dcd0380c727600ada0f0e929
monkstone <martin_p@lineone.net>
2015-07-01 16:25:42 +0100

    refactor empathy

--- contributed/empathy.rb
+++ contributed/empathy.rb
@@ -5,7 +5,7 @@
 # This sketch takes advantage of multiple processors by running calculations
 # in a separate thread.
 
-CELL_COUNT      = 5000
+CELL_COUNT      = 5_000
 SLOW_DOWN       = 0.97
 ROTATION        = 0.004
 LINE_LENGTH     = 37
@@ -16,17 +16,15 @@ attr_reader :cells
 def setup
   sketch_title 'Empathy'
   stroke(0, 0, 0, 25)
-  initialize_cells
+  @cells = create_cells(CELL_COUNT)
   start_cell_updates if MULTI_THREADED
 end
 
-def initialize_cells
-  @cells  = []
-  n = CELL_COUNT
-  n.times do |i|
+def create_cells(n)
+  (0..n).map do |i|
     a = i + rand(PI / 9.0)
     r = ((i / n.to_f) * (width / 2) * (((n - i) / n.to_f) * 3.3)) + rand(6.0)
-    cells << Cell.new((r * cos(a) + width / 2).to_i, (r * sin(a) + height / 2).to_i)
+    Cell.new((r * cos(a) + width / 2).to_i, (r * sin(a) + height / 2).to_i)
   end
 end
 
@@ -36,7 +34,7 @@ end
 
 def draw
   background 255
-  cells.each(&:sense) if started?
+  cells.each(&:draw_line) if started?
 end
 
 def started?
@@ -62,13 +60,13 @@ class Cell
   end
 
   def update
-    det = ((pmouse_x - x) * (mouse_y - y) - (mouse_x - x) * (pmouse_y - y)).to_f
-    @spin += ROTATION * det / dist(x, y, mouse_x, mouse_y).to_f
+    det = ((pmouse_x - x) * (mouse_y - y) - (mouse_x - x) * (pmouse_y - y))
+    @spin += ROTATION * det.to_f / dist(x, y, mouse_x, mouse_y)
     @spin *= SLOW_DOWN
     @angle += spin
   end
 
-  def sense
+  def draw_line
     update unless MULTI_THREADED
     d = LINE_LENGTH * spin + 0.001
     line(x, y, x + d * cos(angle), y + d * sin(angle))

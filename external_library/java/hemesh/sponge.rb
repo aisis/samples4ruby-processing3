8735fcf797fba4874d63fd6cd44c19a08c4d79f8
monkstone <martin_p@lineone.net>
2015-09-22 07:33:54 +0100

    dry and update sponge

--- external_library/java/hemesh/sponge.rb
+++ external_library/java/hemesh/sponge.rb
@@ -1,32 +1,20 @@
 load_library :hemesh
 
-module MS  
+# Module used to include java packages
+module MS
   include_package 'wblut.math'
   include_package 'wblut.processing'
   include_package 'wblut.hemesh'
-  include_package 'wblut.geom'   
-end  
+  include_package 'wblut.geom'
+end
 
-attr_reader :mesh, :render, :points, :num   
+NUM = 50
+attr_reader :mesh, :render, :points
 
 def setup
   sketch_title 'Sponge'
   ArcBall.init(self)
-  rs = MS::WB_RandomOnSphere.new
-  @num = 50
-  @points = MS::WB_Point[50].new
-  50.times.each { |i| points[i] = rs.next_point.mul_self(300.0) }
-  creator = MS::HEC_ConvexHull.new
-  creator.set_points(points)
-  creator.setN(num)
-  @mesh = MS::HE_Mesh.new(creator)
-  @mesh = MS::HE_Mesh.new(MS::HEC_Dual.new(mesh))
-  ext = MS::HEM_Extrude.new.set_chamfer(25).set_relative(false)
-  mesh.modify(ext)
-  sel = ext.extruded
-  ext = MS::HEM_Extrude.new.set_distance(-40)
-  mesh.modify_selected(ext, sel)
-  mesh.smooth(2)
+  create_mesh
   @render = MS::WB_Render3D.new(self)
 end
 
@@ -40,14 +28,16 @@ def draw
   render.draw_faces(mesh)
 end
 
-def mousePressed
+def mouse_pressed
+  create_mesh
+end
+
+def create_mesh
   rs = MS::WB_RandomOnSphere.new
   creator = MS::HEC_ConvexHull.new
-  @num = 50
-  @points = MS::WB_Point[50].new
-  50.times.each { |i| points[i] = rs.next_point.mul_self(300.0) }
+  @points = (0..NUM).map { rs.next_point.mul_self(300.0) }
   creator.set_points(points)
-  creator.setN(num)
+  creator.setN(NUM)
   @mesh = MS::HE_Mesh.new(creator)
   @mesh = MS::HE_Mesh.new(MS::HEC_Dual.new(mesh))
   ext = MS::HEM_Extrude.new.set_chamfer(25).set_relative(false)
@@ -56,7 +46,7 @@ def mousePressed
   ext = MS::HEM_Extrude.new.set_distance(-40)
   mesh.modify_selected(ext, sel)
   mesh.smooth(2)
-end   
+end
 
 def settings
   size(800, 800, P3D)

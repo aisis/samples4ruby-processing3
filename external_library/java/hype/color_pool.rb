f82f873bad2e1e9759f634fad16a6d77ed606a55
monkstone <martin_p@lineone.net>
2016-04-20 20:19:21 +0100

    color_pool

---
+++ external_library/java/hype/color_pool.rb
@@ -0,0 +1,36 @@
+# encoding: utf-8
+# frozen_string_literal: true
+load_library :hype
+include_package 'hype'
+# Access through Hype namespace
+module Hype
+  java_import 'hype.extended.colorist.HColorPool'
+  java_import 'hype.extended.layout.HGridLayout'
+end
+
+WEB_COLORS = %w(#FFFFFF #F7F7F7 #ECECEC #0095A8 #00616F #333333 #FF3300 #FF6600).freeze
+
+def settings
+  size(600, 600)
+end
+
+def setup
+  sketch_title 'Color Pool'
+  H.init(self)
+  H.background(color('#242424'))
+  colors = Hype::HColorPool.new
+  WEB_COLORS.each do |col|
+    colors.add(color(col))
+  end
+  pool = HDrawablePool.new(15_876)
+  pool.auto_add_to_stage
+      .add(HRect.new(5))
+      .layout(Hype::HGridLayout.new.start_x(5).start_y(5).spacing(5, 5).cols(126))
+      .on_create do |obj|
+        i = pool.current_index
+        d = obj.to_java(Java::Hype::HDrawable)
+        d.no_stroke.fill(colors.get_color(i))
+      end
+      .request_all
+  H.draw_stage
+end

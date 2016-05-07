a9cc70d77b2843222a87e842ef4a12991c5c77d3
monkstone <martin_p@lineone.net>
2016-05-07 09:08:18 +0100

    examples for perspektiv method

--- processing_app/basics/camera/perspektiv.rb
+++ processing_app/basics/camera/perspektiv.rb
@@ -1,16 +1,16 @@
 # Move the mouse left and right to change the field of view (fov).
-# Click to modify the aspect ratio. The perspective method
+# Click to modify the aspect ratio. The perspektiv method
 # sets a perspective projection applying foreshortening, making 
 # distant objects appear smaller than closer ones. The parameters 
 # define a viewing volume with the shape of truncated pyramid. 
 # while farther objects appear smaller. This projection simulates 
 # the perspective of the world more accurately than orthographic projection. 
 # The version of perspective without parameters sets the default 
-# perspective and the version with four parameters allows the programmer 
+# perspective and the version with up to four parameters allows the programmer 
 # to set the area precisely.
 
 def setup
-  sketch_title 'Perspective'
+  sketch_title 'Perspektiv Example'
   no_stroke
 end
 
@@ -22,7 +22,12 @@ def draw
   camera_z = camera_y / Math.tan(fov / 2.0)
   aspect = width.to_f / height 
   aspect /= 2.0 if mouse_pressed?  
-  perspective(fov, aspect, camera_z / 10.0, camera_z * 10.0)  
+  perspektiv(
+    fov: fov, 
+    aspect_ratio: aspect, 
+    near_z: camera_z / 10.0, 
+    far_z: camera_z * 10.0
+  )  
   translate width / 2.0 + 30, height / 2.0, 0
   rotate_x -PI / 6
   rotate_y PI / 3 + mouse_y / height.to_f * PI
